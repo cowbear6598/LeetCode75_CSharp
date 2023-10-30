68b0a91958d375b3e6c5eec99666fa603ca3fa53
CptSoap <cowbear6598@gmail.com>
2023-10-31 07:26:07 +0800

    feat: complete KidsWithCandies 1031

---
+++ LeetCode75/KidsWithTheGreatestNumberOfCandies/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode75.KidsWithTheGreatestNumberOfCandies
+{
+    public class Solution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int    greatestNumber = candies.Max();
+            bool[] results        = new bool[candies.Length];
+
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= greatestNumber)
+                {
+                    results[i] = true;
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ LeetCode75/KidsWithTheGreatestNumberOfCandies/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCode75.KidsWithTheGreatestNumberOfCandies
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase(new[] { true, true, true, false, true }, new[] { 2, 3, 5, 1, 3 }, 3)]
+        [TestCase(new[] { true, false, false, false, false }, new[] { 4, 2, 1, 1, 2 }, 1)]
+        [TestCase(new[] { true, false, true }, new[] { 12, 1, 12 }, 10)]
+        public void Should_Correct(bool[] expected, int[] candies, int extraCandies)
+        {
+            var solution = new Solution();
+
+            var results = solution.KidsWithCandies(candies, extraCandies).ToArray();
+
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
