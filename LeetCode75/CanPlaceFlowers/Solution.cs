1e6fbc7ef2f8af71f75605de8b14620afadb4f79
CptSoap <cowbear6598@gmail.com>
2023-11-01 08:35:29 +0800

    feat: complete can place flowers

---
+++ LeetCode75/CanPlaceFlowers/Solution.cs
@@ -0,0 +1,81 @@
+﻿namespace LeetCode75.CanPlaceFlowers
+{
+    public class Solution
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            var prev = 0;
+            var next = 0;
+
+            for (var i = 0; i < flowerbed.Length; i++)
+            {
+                if (n == 0)
+                {
+                    return true;
+                }
+
+                if (i != flowerbed.Length - 1)
+                {
+                    next = flowerbed[i + 1];
+                }
+
+                if (prev == 0 && next == 0 && flowerbed[i] == 0)
+                {
+                    n--;
+                    flowerbed[i] = 1;
+                }
+
+                prev = flowerbed[i];
+            }
+
+            return n == 0;
+        }
+        
+        // public bool CanPlaceFlowers(int[] flowerbed, int n)
+        // {
+        //     for (var i = 0; i < flowerbed.Length; i++)
+        //     {
+        //         if (n == 0)
+        //         {
+        //             return true;
+        //         }
+        //
+        //         if (flowerbed[i] == 1)
+        //         {
+        //             continue;
+        //         }
+        //
+        //         if (flowerbed.Length == 1)
+        //         {
+        //             PlaceFlower(ref flowerbed, ref n, i);
+        //             continue;
+        //         }
+        //
+        //         if (CanPlace(flowerbed, i + 1) && CanPlace(flowerbed, i - 1))
+        //         {
+        //             PlaceFlower(ref flowerbed, ref n, i);
+        //         }
+        //     }
+        //
+        //     return n == 0;
+        // }
+        //
+        // private void PlaceFlower(ref int[] flowerbed, ref int n, int i)
+        // {
+        //     flowerbed[i] = 1;
+        //     n--;
+        // }
+        //
+        // private bool CanPlace(int[] flowerbed, int i)
+        // {
+        //     try
+        //     {
+        //         return flowerbed[i] == 0;
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return true;
+        //     }
+        // }
+    }
+}
---
+++ LeetCode75/CanPlaceFlowers/Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode75.CanPlaceFlowers
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase(true, new[] { 1, 0, 0, 0, 1 }, 1)]
+        [TestCase(false, new[] { 1, 0, 0, 0, 1 }, 2)]
+        [TestCase(true, new[] { 0 }, 1)]
+        [TestCase(true, new[] { 0, 0, 1, 0, 0 }, 1)]
+        public void Should_Correct(bool expected, int[] flowerbed, int n)
+        {
+            var solution = new Solution();
+
+            var result = solution.CanPlaceFlowers(flowerbed, n);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
