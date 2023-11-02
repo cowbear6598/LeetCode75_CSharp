2c847a537b4789a9f05a05598346a5f2565b839d
cowbear6598 <cowbear6598@gmail.com>
2023-11-02 10:00:01 +0800

    feat: complete ReverseWords 20231102

---
+++ LeetCode75/ReverseWordsInAString/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode75.ReverseWordsInAString
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var words = s.Split(' ');
+
+            var result = new StringBuilder();
+
+            for (var i = words.Length - 1; i >= 0; i--)
+            {
+                if (string.IsNullOrWhiteSpace(words[i]))
+                {
+                    continue;
+                }
+
+                result.Append(words[i]);
+
+                if (i != 0)
+                {
+                    result.Append(" ");
+                }
+            }
+
+            if (result[result.Length - 1] == ' ')
+            {
+                result.Remove(result.Length - 1, 1);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCode75/ReverseWordsInAString/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode75.ReverseWordsInAString
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase("blue is sky the", "the sky is blue")]
+        [TestCase("world hello", "  hello world  ")]
+        [TestCase("example good a", "a good   example")]
+        public void Should_Correct(string expected, string s)
+        {
+            Solution solution = new Solution();
+
+            string result = solution.ReverseWords(s);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
