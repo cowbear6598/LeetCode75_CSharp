0c19dbaea4a07a4af880c19a91a0063ee0c591db
CptSoap <cowbear6598@gmail.com>
2023-11-02 08:38:06 +0800

    working: optimize anwser

---
+++ LeetCode75/ReverseVowelsOfAString/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode75.ReverseVowelsOfAString
+{
+    public class Solution
+    {
+        private char[] vowels = { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+
+        public string ReverseVowels(string s)
+        {
+            var result = new StringBuilder();
+            result.EnsureCapacity(s.Length);
+
+            var b = s.Length - 1;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var currentValue = s[i];
+
+
+                if (!vowels.Contains(currentValue) || b == 0 || i >= b)
+                {
+                    result.Append(currentValue);
+                    continue;
+                }
+
+                for (var j = b; j >= 0; j--)
+                {
+                    var reverseValue = s[j];
+
+                    if (!vowels.Contains(reverseValue) || j < i)
+                        continue;
+
+                    result.Append(reverseValue);
+                    b = j - 1;
+                    break;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCode75/ReverseVowelsOfAString/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode75.ReverseVowelsOfAString
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase("holle", "hello")]
+        [TestCase("leotcede", "leetcode")]
+        public void Should_Correct(string expected, string s)
+        {
+            Solution solution = new Solution();
+
+            var result = solution.ReverseVowels(s);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
